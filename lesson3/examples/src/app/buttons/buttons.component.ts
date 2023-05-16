1b388cb47cca397127438bb89b6ec8cbae25274d
Polina Bartles <polina.bartles@gmail.com>
2023-05-15 20:06:31 -0500

    Single button click and reset

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   inactiveGold: boolean = false;
+   inactiveSilver: boolean = false;
+   inactiveCopper: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
+
+   setButtonInactive(buttonId: string) {
+    switch (buttonId) {
+      case 'gold':
+        this.inactiveGold = true;
+        break;
+      case 'silver':
+        this.inactiveSilver = true;
+        break;
+      case 'copper':
+      this.inactiveCopper = true;
+      break;
+      default:
+        break;
+    }
+   }
+
+   resetButtonStates() {
+    this.inactiveGold = false;
+    this.inactiveSilver = false;
+    this.inactiveCopper = false;
+
+   }
 
 }
